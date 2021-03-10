3aa4250b6d79defd609b59b15d001154c4ce06d2
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-03-10 22:10:25 +0300

    deploying...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const path = require('path');
 
 
 const app = express();
@@ -24,5 +25,15 @@ const PORT = process.env.PORT || 5000;
 // Use Routes
 app.use('/api/users', require('./routes/api/Users'));
 app.use('/api/auth', require('./routes/api/Auth'));
+
+// Serve static assets if in prduction
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => `Server running on port ${PORT}`);
