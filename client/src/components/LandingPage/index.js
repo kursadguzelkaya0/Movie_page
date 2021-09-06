56ad9d0df861872c54303364132989dc50a1f00b
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-09-06 20:54:04 +0300

    fix(landingPage): add login register modals to buttons

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Login from '../Auth/Login';
+import RegisterModal from '../Auth/RegisterModal';
 import Footer from '../Footer/Footer';
 import AppNavbar from '../Navbar/AppNavbar';
 import './LandingPage.css';
@@ -12,8 +14,8 @@ const LandingPage = () => {
           <span className="landing-info-header">MovieBest</span>
           <span className="landing-info-body">Find Best Movies Easily</span>
           <span className="landing-info-description">To find information about movies please sign up or sign in</span>
-          <button className="button sign-in" type="button" onClick={() => console.log('CLicked')}>Sign Up</button>
-          <button className="button sign-in" type="button" onClick={() => console.log('CLicked')}>Sign In</button>
+          <button className="button sign-in" type="button" onClick={() => console.log('CLicked')}><Login /></button>
+          <button className="button sign-in" type="button" onClick={() => console.log('CLicked')}><RegisterModal /></button>
         </div>
         <Footer />
       </div>
